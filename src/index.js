fdc137470afe881e5f575d879c78301724510c34
KrishnaThakur10 <krishnathakur27082005@gmail.com>
2025-08-15 14:32:46 +0530

    successfully connected mongodb

--- src/index.js
+++ src/index.js
@@ -1,21 +1,31 @@
-import mongoose from "mongoose";
-import { DB_NAME } from "./constants.js";
-import express from 'express'
+// import mongoose from "mongoose";
+// import { DB_NAME } from "./constants.js";
+// import express from 'express'
+import connectDB from "./db/index.js";
+import dotenv from 'dotenv';
 
-const app = express()
+dotenv.config()
 
-(async() => 
-    {
-        try {
-            await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
-            app.on("ERROR",(error)=>{
-                console.log("Express is not getting connect: ", error);
-                throw error
-            })
-            app.listen()
-        } catch (error) {
-            console.error("Error is: ",error);
-            throw error
+connectDB()
+
+
+// const app = express()
+
+// (async() => 
+//     {
+//         try {
+//             await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+//             app.on("ERROR",(error)=>{
+//                 console.log("Express is not getting connect: ", error);
+//                 throw error
+//             })
+//             app.listen(process.env.PORT, ()=>{
+//                 console.log(`App is listening on this port:${process.env.PORT}`);
+                
+//             } )
+//         } catch (error) {
+//             console.error("Error is: ",error);
+//             throw error
             
-        }
-    })()
+//         }
+//     })()
