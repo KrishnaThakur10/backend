e6987ee0fcc99bb34b287a1ead61e05e20a47be5
KrishnaThakur10 <krishnathakur27082005@gmail.com>
2025-08-16 17:37:32 +0530

    tried new app usage

--- src/index.js
+++ src/index.js
@@ -2,16 +2,28 @@
 // import { DB_NAME } from "./constants.js";
 // import express from 'express'
 import connectDB from "./db/index.js";
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
+import app from "./app.js";
 
-dotenv.config()
+dotenv.config();
 
 connectDB()
+  .then(() => {
+    app.on("error", (error) => {
+      console.log("error", error);
+    });
 
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`App is listening on this port: ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log(`Connection Failed : `, err);
+  });
 
 // const app = express()
 
-// (async() => 
+// (async() =>
 //     {
 //         try {
 //             await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
@@ -21,11 +33,11 @@ connectDB()
 //             })
 //             app.listen(process.env.PORT, ()=>{
 //                 console.log(`App is listening on this port:${process.env.PORT}`);
-                
+
 //             } )
 //         } catch (error) {
 //             console.error("Error is: ",error);
 //             throw error
-            
+
 //         }
 //     })()
